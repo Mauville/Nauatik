06b3e7a38044267c2231d344e6cfec26d4309460
Mauville <4991127@outlook.com>
2020-10-30 22:30:57 -0600

    Add translation and crapola animation

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,15 +1,35 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
+
+// A bad attempt at a button animation. I hate animating
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
+  animations: [
+    trigger('buttonState', [
+      state('active', style({
+        color: '#384C1C',
+      })),
+      state('inactive', style({
+        backgroundColor: '#496225',
+      })),
+      transition('inactive <=> active', animate('100ms ease-in')),
+    ])
+  ]
 })
 export class ButtonComponent implements OnInit {
+
+  state = 'inactive';
 
   @Input() buttonConfig: any;
 
   constructor() {
+  }
+
+  toggleState(): void {
+    this.state = this.state === 'active' ? 'inactive' : 'active';
   }
 
   ngOnInit(): void {
